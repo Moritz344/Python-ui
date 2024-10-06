3e004308db48316cf04e2d0fd69ba04bb6806c5d
Moritz <157284909+Moritz344@users.noreply.github.com>
2024-10-06 15:12:40 +0200

    Update TODO.py

--- TODO.py
+++ TODO.py
@@ -2,6 +2,8 @@ from customtkinter import *
 from tkinter import font
 import customtkinter as ctk
 from datetime import datetime
+from tkinter import PhotoImage
+from PIL import Image
 
 class Task():
      def __init__(self,master):
@@ -13,12 +15,21 @@ class Task():
           self.button_color = "light blue"
           self.clicked_color = "lightcoral"
           self.hover_color = "lightgreen"
-
+          
           self.tasks = []
+          try:
+               image = Image.open("trash.png")
+               trash_icon = CTkImage(image)
+          except Exception as e:
+               print(e)
 
           self.Button = CTkButton(master,text="New Task",fg_color=self.button_color,text_color="black")
           self.Button.grid(column=0,row=0,padx=630,pady=10,sticky="nw")
           self.Entry = CTkEntry(master,corner_radius=5)
+          self.Entry_delete = CTkEntry(master,corner_radius=5)
+
+          self.delete_button = CTkButton(master,text="Delete",image=trash_icon,command=self.remove_task)
+          self.delete_button.grid(row=0,column=0,padx=450,pady=10,sticky="nw")
 
           #self.task = CTkLabel(master,text="Homework",fg_color="transparent",font=CTkFont(family="Arial",size=20))
 
@@ -31,20 +42,33 @@ class Task():
           self.task_row = 0
           self.value = 1
           self.time = datetime.now().strftime("%Y-%m-%d")
+          
      def show_entry(self):
           self.Entry.grid(column=0,row=0,padx=0,pady=50,sticky="n")
+     def show_delete_entry(self):
+          self.Entry_delete.grid(column=0,row=0,padx=450,pady=50,sticky="nw")
      def on_click(self,event):
           self.add_task()
      def on_added(self,event):
           self.add_task()
      def add_task(self):
           self.show_entry()
-          selected_item = self.Entry.get()
-          if selected_item:
-               self.tasks.append(selected_item)
+          self.selected_item = self.Entry.get()
+          if self.selected_item:
+               self.tasks.append(self.selected_item)
                self.Entry.delete(0,"end")
                self.update_task_list()
-               print(selected_item) 
+               print(self.selected_item) 
+     def remove_task(self):
+          self.show_delete_entry()
+          selected_item_delete = self.Entry_delete.get()
+          if selected_item_delete in self.tasks: 
+               self.Entry_delete.delete(0,"end")
+               self.tasks.remove(selected_item_delete)
+               self.update_task_list()
+          else:
+               print("Task does not exist.")
+
      def update_task_list(self):
           for widget in self.TaskListFrame.winfo_children():
                widget.destroy()
@@ -54,19 +78,16 @@ class Task():
                date_label = CTkLabel(self.TaskListFrame,text=self.time,fg_color="transparent",font=CTkFont(family="Arial",size=15),text_color="grey")
                check_box = CTkCheckBox(self.TaskListFrame,text=None,corner_radius=3)
 
-               task_label.grid(row=self.task_row,column=2,padx=10,pady=10) 
-               date_label.grid(row=self.task_row ,column=3,padx=0,pady=10,sticky="we")
-               check_box.grid(row=self.task_row,column=0,sticky="w")
+               task_label.grid(row=self.task_row,column=1,padx=10,pady=0,sticky="w") 
+               date_label.grid(row=self.task_row ,column=3,padx=10,pady=10,sticky="we")
+               check_box.grid(row=self.task_row,column=4,sticky="w")
                self.task_row += self.value 
 
-          if self.task_row > 7:
-               self.tasks.remove(task)
-
+          
 
 window = ctk.CTk()
 window.geometry("800x600")
 window.title("TODO")
 window.resizable(False,False)
 task = Task(window)
-
 window.mainloop()
